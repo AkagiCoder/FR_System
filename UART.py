bb66436e4951382ad8e786657b92bf4f76652a12
AkagiCoder <35548639+AkagiCoder@users.noreply.github.com>
2020-01-27 19:28:42 -0800

    Remove unnecessary test files

--- UART.py
+++
@@ -1,9 +0,0 @@
-import serial
-from time import sleep
-
-serialport = serial.Serial("/dev/ttyS0", baudrate = 9600)
-
-while True:
-    #serialport.write("Hello")
-    x = serialport.readline()
-    print(x)
