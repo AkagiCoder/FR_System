ec0b39c3510393a7fed43092a03d4708018957d7
C9Adrian <adrianruizsolorio@gmail.com>
2020-03-08 22:03:42 -0700

    Email and Research added

---
+++ KED_Code/email_face.py
@@ -0,0 +1,62 @@
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+   
+fromaddr = "adrianruizsolorio@gmail.com"
+toaddr = "ruiza12@unlv.nevada.edu"
+   
+# instance of MIMEMultipart 
+msg = MIMEMultipart() 
+  
+# storing the senders email address   
+msg['From'] = fromaddr 
+  
+# storing the receivers email address  
+msg['To'] = toaddr 
+  
+# storing the subject  
+msg['Subject'] = "Testing the Email"
+  
+# string to store the body of the mail 
+body = "I attached an anime girl pic"
+  
+# attach the body with the msg instance 
+msg.attach(MIMEText(body, 'plain')) 
+  
+# open the file to be sent  
+filename = "tiredgirl.jpg"
+attachment = open("C:/Users/Adrian/Desktop/testFR/tiredgirl.jpg", "rb") 
+  
+# instance of MIMEBase and named as p 
+p = MIMEBase('application', 'octet-stream') 
+  
+# To change the payload into encoded form 
+p.set_payload((attachment).read()) 
+  
+# encode into base64 
+encoders.encode_base64(p) 
+   
+p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+  
+# attach the instance 'p' to instance 'msg' 
+msg.attach(p) 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login(fromaddr, "adrianruiz1") 
+  
+# Converts the Multipart msg into a string 
+text = msg.as_string() 
+  
+# sending the mail 
+s.sendmail(fromaddr, toaddr, text) 
+  
+# terminating the session 
+s.quit() 
