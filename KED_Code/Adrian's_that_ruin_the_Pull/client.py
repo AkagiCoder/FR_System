26f6f9b194af43984140e88473f8a99a113d8a21
C9Adrian <adrianruizsolorio@gmail.com>
2020-05-05 14:25:47 -0700

    updated the socket code

--- KED_Code/Adrian's_that_ruin_the_Pull/client.py
+++ KED_Code/Adrian's_that_ruin_the_Pull/client.py
@@ -1,11 +1,27 @@
 import socket
+import pickle
+from PIL import Image
 
-HOST = '192.168.1.21'  # The server's hostname or IP address
-PORT = 5689        # The port used by the server
+HOST = '192.168.1.28'  # The server's hostname or IP address
+PORT = 5678        # The port used by the server
 
 HEADERSIZE = 10
 
 s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+full_msg = b''
+while True:
+	msg = s.recv(1024)
+	if len(msg) <= 0:
+		break
+	full_msg += msg
+
+breakIn = full_msg
+print(breakIn[0:37])
+newMsg = breakIn[37:]
+#print(newMsg)
+
+encode_img = pickle.loads(newMsg)
+encode_img.show()
 s.close()
 
