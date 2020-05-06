88422d6e4b19398479e4fb7fc97c464555e41a97
C9Adrian <adrianruizsolorio@gmail.com>
2020-05-05 17:39:51 -0700

    Added Key Type, Key and Break in Socket, and Email

--- KED_Code/client.py
+++ KED_Code/client.py
@@ -2,13 +2,10 @@ import socket
 import pickle
 from PIL import Image
 
-HOST = socket.gethostname()
-print(HOST)
-
-'''
 HOST = '192.168.1.19'  # The server's hostname or IP address
 PORT = 4567        # The port used by the server
- 
+
+
 s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 full_msg = b''
@@ -18,15 +15,18 @@ while True:
 		break
 	full_msg += msg
 
-'''
-'''
-breakIn = full_msg
-print(breakIn[0:37])
-newMsg = breakIn[37:]
-#print(newMsg)
+if len(full_msg) < 100:
+	print("New Key Creation at Address 1234")
+	print("Key\tCreation Date & Time\tExpiration Date & Time \tType")
+	print(full_msg.decode('utf-8'))
 
-encode_img = pickle.loads(newMsg)
-encode_img.show()
-'''
+else:
+	breakIn = full_msg
+	print(breakIn[0:37])
+	newMsg = breakIn[37:]
+	#print(newMsg)
+
+	encode_img = pickle.loads(newMsg)
+	encode_img.show()
 s.close()
 
